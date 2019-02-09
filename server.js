b63e84acb1a91ddc5cb7b61bf3a20403105b9c9a
Sabrina Sukhin <sabrina@sukhin.com>
2019-02-09 00:06:12 -0500

    finish directory getter

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const textParser = require("./text-parser/text-parser");
+const dictionaryGetter = require("./text-parser/DirectoryGetter");
 
 server.listen(PORT);
 
@@ -16,5 +17,9 @@ io.on("connection", function (socket) {
   socket.on("textData", function (data) {
     // This is called when the user sends a text input
     console.log(textParser.ParseKeywords(data));
+    dictionaryGetter.andrewidLookup(data, (result) => {
+      console.log(result);
+      socket.emit("result", result || textParser.ParseKeywords(data));
+    });
   });
 });
