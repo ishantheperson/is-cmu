6b1ebb16deac4db452b66613025996ec5279f4f3
Ishan Bhargava <ishantheperson@users.noreply.github.com>
2019-02-09 11:10:52 -0500

    Added status emitter to DictionaryGetter

--- text-parser/DirectoryGetter.js
+++ text-parser/DirectoryGetter.js
@@ -4,7 +4,8 @@ module.exports = {
   andrewidLookup: andrewidLookup
 };
 
-function andrewidLookup(input, callback) {
+function andrewidLookup(input, callback, emitStatus) {
+  emitStatus("Sending request to ScottyLabs...");
   https.get(`https://apis.scottylabs.org/directory/v1/andrewID/${input}`, (resp) => {
     let data = '';
 
@@ -15,6 +16,8 @@ function andrewidLookup(input, callback) {
 
     // The whole response has been received. Print out the result.
     resp.on('end', () => {
+      emitStatus("Received response. Parsing...");
+
       try {
         callback(true, JSON.parse(data));
       }
