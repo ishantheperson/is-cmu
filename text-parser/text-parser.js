0063a63d14a1f9ef3f48d2ecfbe3de2d1ff1456a
Sabrina Sukhin <sabrina@sukhin.com>
2019-02-08 22:39:06 -0500

    optimize TextParser

--- text-parser/text-parser.js
+++ text-parser/text-parser.js
@@ -3,10 +3,34 @@ module.exports = {
     ParseKeywords: ParseKeywords
 };
 function ParseKeywords(input) {
+    let inputArray = input.split(' ').map((word) => word.toLowerCase());
     const keywords = fs.readFileSync("text-parser/isCmuKeywords.txt").toString();
     const splitted = keywords.split('\r\n').map((word) => word.toLowerCase());
-    if (splitted.indexOf(input.toLowerCase().trim()) !== -1) {
-        return true;
+    const splittedAgain = splitted.map((word) => word.split(' '));
+    /*for (let c of splitted)
+    {
+      splittedAgain[] = c.split(' ').map((word) => word.toLowerCase());
+    }*/
+    for (let i = 0; i < inputArray.length; i++) {
+        //console.log("input");
+        for (let phrase of splittedAgain) {
+            let failed = false;
+            //console.log("phrase");
+            if (phrase.length === 0) {
+                //console.log("oof");
+            }
+            for (let wordCount = 0; wordCount < phrase.length; wordCount++) {
+                //console.log(inputArray[i]);
+                if (i + wordCount >= inputArray.length || phrase[wordCount] !== inputArray[i + wordCount]) {
+                    //console.log("yeet");
+                    failed = true;
+                    break;
+                }
+            }
+            if (!failed) {
+                return true;
+            }
+        }
     }
     return false;
 }
--- text-parser/text-parser.ts
+++ text-parser/text-parser.ts
@@ -6,13 +6,40 @@ module.exports = {
 
 function ParseKeywords(input: string): boolean
 {
+  let inputArray: string[] = input.split(' ').map((word) => word.toLowerCase());
   const keywords = fs.readFileSync("text-parser/isCmuKeywords.txt").toString();
   const splitted: string[] = keywords.split('\r\n').map((word) => word.toLowerCase());
-  
-
-  if (splitted.indexOf(input.toLowerCase().trim()) !== -1)
+  const splittedAgain: string[][] = splitted.map((word) => word.split(' '));
+  /*for (let c of splitted)
   {
-    return true;
+    splittedAgain[] = c.split(' ').map((word) => word.toLowerCase());
+  }*/
+  for (let i = 0; i < inputArray.length; i++)
+  {
+    //console.log("input");
+    for (let phrase of splittedAgain)
+    {
+      let failed : boolean = false;
+      //console.log("phrase");
+      if(phrase.length === 0)
+      {
+        //console.log("oof");
+      }
+      for (let wordCount = 0; wordCount < phrase.length; wordCount++)
+      {
+        //console.log(inputArray[i]);
+        if(i + wordCount >= inputArray.length || phrase[wordCount] !== inputArray[i + wordCount])
+        {
+          //console.log("yeet");
+          failed = true;
+          break;
+        }
+      }
+      if(!failed)
+      {
+        return true;
+      }
+    }
   }
   return false;
 }
