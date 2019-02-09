39b51e22c9121e5589a72640179345679d1a3676
bertalay <alu@imsa.edu>
2019-02-08 22:58:18 -0500

    Added DirectoryGetter

---
+++ text-parser/DirectoryGetter.js
@@ -0,0 +1,21 @@
+const https = require('https');
+
+
+
+
+https.get('https://apis.scottylabs.org/directory/v1/andrewID/b', (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data));
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
