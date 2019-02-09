efc95fef8f424c2fc15a64e4bc8be614b57a6300
Sabrina Sukhin <sabrina@sukhin.com>
2019-02-08 23:17:24 -0500

    start DirectoryGetter

--- text-parser/DirectoryGetter.js
+++ text-parser/DirectoryGetter.js
@@ -1,21 +1,28 @@
 const https = require('https');
 
+module.exports = {
+  andrewidLookup: andrewidLookup
+};
 
-
-
-https.get('https://apis.scottylabs.org/directory/v1/andrewID/b', (resp) => {
+function andrewidLookup(input)
+{
+  https.get(`https://apis.scottylabs.org/directory/v1/andrewID/${input}`, (resp) => {
   let data = '';
 
+  
   // A chunk of data has been recieved.
   resp.on('data', (chunk) => {
     data += chunk;
   });
-
+  console.log("here");
   // The whole response has been received. Print out the result.
   resp.on('end', () => {
     console.log(JSON.parse(data));
   });
 
-}).on("error", (err) => {
+  }).on("error", (err) => {
   console.log("Error: " + err.message);
-});
+  });
+}
+
+andrewidLookup("poop");
