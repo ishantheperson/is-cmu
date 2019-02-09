e2e435466b5264dbffcb84bbdb68ddf17f1ee5e8
Sabrina Sukhin <sabrina@sukhin.com>
2019-02-08 22:41:07 -0500

    finish textParser

--- text-parser/text-parser.js
+++ text-parser/text-parser.js
@@ -7,22 +7,11 @@ function ParseKeywords(input) {
     const keywords = fs.readFileSync("text-parser/isCmuKeywords.txt").toString();
     const splitted = keywords.split('\r\n').map((word) => word.toLowerCase());
     const splittedAgain = splitted.map((word) => word.split(' '));
-    /*for (let c of splitted)
-    {
-      splittedAgain[] = c.split(' ').map((word) => word.toLowerCase());
-    }*/
     for (let i = 0; i < inputArray.length; i++) {
-        //console.log("input");
         for (let phrase of splittedAgain) {
             let failed = false;
-            //console.log("phrase");
-            if (phrase.length === 0) {
-                //console.log("oof");
-            }
             for (let wordCount = 0; wordCount < phrase.length; wordCount++) {
-                //console.log(inputArray[i]);
                 if (i + wordCount >= inputArray.length || phrase[wordCount] !== inputArray[i + wordCount]) {
-                    //console.log("yeet");
                     failed = true;
                     break;
                 }
--- text-parser/text-parser.ts
+++ text-parser/text-parser.ts
@@ -10,27 +10,16 @@ function ParseKeywords(input: string): boolean
   const keywords = fs.readFileSync("text-parser/isCmuKeywords.txt").toString();
   const splitted: string[] = keywords.split('\r\n').map((word) => word.toLowerCase());
   const splittedAgain: string[][] = splitted.map((word) => word.split(' '));
-  /*for (let c of splitted)
-  {
-    splittedAgain[] = c.split(' ').map((word) => word.toLowerCase());
-  }*/
+
   for (let i = 0; i < inputArray.length; i++)
   {
-    //console.log("input");
     for (let phrase of splittedAgain)
     {
       let failed : boolean = false;
-      //console.log("phrase");
-      if(phrase.length === 0)
-      {
-        //console.log("oof");
-      }
       for (let wordCount = 0; wordCount < phrase.length; wordCount++)
       {
-        //console.log(inputArray[i]);
         if(i + wordCount >= inputArray.length || phrase[wordCount] !== inputArray[i + wordCount])
         {
-          //console.log("yeet");
           failed = true;
           break;
         }
